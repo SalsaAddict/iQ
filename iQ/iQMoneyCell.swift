b51ee6db405b842e58b6a130e747f1a05ae1f4ad
Ws271014 <jonathan.clarke@whitespace.co.uk>
2015-02-12 15:40:54 +0000

    Adding iQMoneyCell

--- iQ/iQMoneyCell.swift
+++ iQ/iQMoneyCell.swift
@@ -1,5 +1,5 @@
 //
-//  iQNumericTextCell.swift
+//  iQMoneyCell.swift
 //  iQ
 //
 //  Created by Jonathan Clarke on 12/02/2015.
@@ -8,15 +8,16 @@
 
 import UIKit
 
-class iQNumericTextCell: iQTextCell {
+class iQMoneyCell: iQTextCell {
     
-    override class var reuseIdentifier: String { get { return "iQNumericTextCell" } }
+    override class var reuseIdentifier: String { get { return "iQMoneyCell" } }
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .Default, reuseIdentifier: reuseIdentifier)
         self.textField.autocorrectionType = .No
         self.textField.autocapitalizationType = .None
         self.textField.keyboardType = UIKeyboardType.NumbersAndPunctuation
+        self.textField.textAlignment = NSTextAlignment.Right
     }
     
     required init(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
